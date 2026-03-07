c5f764ee075584fc25c4b8a91a863a809d44d12f
Alice Pidd <alicempidd@gmail.com>
2026-03-07 12:41:29 +1000

    Cleaning up scripts

--- 6_climate_acceleration_latitude_plot.R
+++ 6_climate_acceleration_latitude_plot.R
@@ -1,41 +1,14 @@
 # Plotting climate acceleration by 1° latitude
-  # Written by Alice P
-    # 6 March 2026
+# Written by Alice P
+# 6 March 2026
 
 
 
 # Questions:
-  # Acceleration at each 1° of latitude - median of each band, per SSP (no time aspect)
-  # Acceleration within MPAs vs. outside MPAs - yearly median timeseries
-  # Acceleration per SSP, 
+# Acceleration at each 1° of latitude - median of each band, per SSP (no time aspect)
+# Acceleration within MPAs vs. outside MPAs - yearly median timeseries
+# Acceleration per SSP, 
 
-
-
-
-# Helpers ----------------------------------------------------------------------
-
-  source("Helpers.R")
-  source_disk <- "/Volumes/AliceShield/acceleration_data"
-  source("Background_data.R")
-  
-  
-  
-  
-# Folders ----------------------------------------------------------------------
-
-  acc_fol <- make_folder(source_disk, "3_acceleration_aus", "") # Aus files
-  plot_fol <- make_folder(source_disk, "4_acceleration_aus_plot", "") # Aus files
-  
-  # acc_global_fol <- make_folder(source_disk, "3_acceleration_global", "") # Global files
-  
-  
-  
-  
-# Get files --------------------------------------------------------------------
-  
-  files <- dir(acc_fol, full.names = TRUE)
-  files
-  
   f <- files[1]
   
   
---
+++ 7_climate_acceleration_spatial_plot.R
@@ -0,0 +1,10 @@
+# Plotting climate acceleration spatially
+  # Written by Alice P
+    # 6 March 2026
+
+
+
+# Questions:
+  # Acceleration at each 1° of latitude - median of each band, per SSP (no time aspect)
+  # Acceleration within MPAs vs. outside MPAs - yearly median timeseries
+  # Acceleration per SSP, 
